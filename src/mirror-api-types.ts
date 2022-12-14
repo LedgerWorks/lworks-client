315f9a7b7e6f76a5c64a143a5c45771d41d5bda8
Chad Schwab <chad.schwab@lworks.io>
2022-12-14 16:09:29 -0600

    add all response types to mirror types

--- src/mirror-api-types.ts
+++ src/mirror-api-types.ts
@@ -4,3 +4,25 @@ export type TransactionsResponse = components["schemas"]["TransactionsResponse"]
 
 export type HCSTopicMessagesResponse = components["schemas"]["TopicMessagesResponse"];
 export type HCSTopicMessage = components["schemas"]["TopicMessage"];
+
+export type AccountsResponse = components["schemas"]["AccountsResponse"];
+export type BalancesResponse = components["schemas"]["BalancesResponse"];
+export type ContractsResponse = components["schemas"]["ContractsResponse"];
+export type ContractResultResponse = components["schemas"]["ContractResultResponse"];
+export type ContractResultsResponse = components["schemas"]["ContractResultsResponse"];
+export type ContractActionsResponse = components["schemas"]["ContractActionsResponse"];
+export type ContractLogsResponse = components["schemas"]["ContractLogsResponse"];
+export type CryptoAllowancesResponse = components["schemas"]["CryptoAllowancesResponse"];
+export type NetworkExchangeRateSetResponse =
+  components["schemas"]["NetworkExchangeRateSetResponse"];
+export type NetworkFeesResponse = components["schemas"]["NetworkFeesResponse"];
+export type NetworkNodesResponse = components["schemas"]["NetworkNodesResponse"];
+export type NetworkStakeResponse = components["schemas"]["NetworkStakeResponse"];
+export type NetworkSupplyResponse = components["schemas"]["NetworkSupplyResponse"];
+export type SchedulesResponse = components["schemas"]["SchedulesResponse"];
+export type BlocksResponse = components["schemas"]["BlocksResponse"];
+export type TokenAllowancesResponse = components["schemas"]["TokenAllowancesResponse"];
+export type TokenBalancesResponse = components["schemas"]["TokenBalancesResponse"];
+export type TokensResponse = components["schemas"]["TokensResponse"];
+export type TopicMessagesResponse = components["schemas"]["TopicMessagesResponse"];
+export type TransactionByIdResponse = components["schemas"]["TransactionByIdResponse"];
