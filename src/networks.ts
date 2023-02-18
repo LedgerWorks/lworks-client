217b2f24062770cc91df9c0aeb4273e1aead0172
Cory Huebner <chuebner86@gmail.com>
2023-02-18 11:18:41 -0600

    DEV-1709 - drop unused map of network=>urls

--- src/networks.ts
+++ src/networks.ts
@@ -6,13 +6,3 @@ export enum Network {
 const networkValues = Object.values(Network);
 
 export type NetworkNames = typeof networkValues;
-
-export const NetworkHostMap: Record<Network, string> = {
-  [Network.Mainnet]: "https://mainnet.mirror.lworks.io",
-  [Network.Testnet]: "https://testnet.mirror.lworks.io",
-};
-
-export const NetworkHostMapForSentinel: Record<Network, string> = {
-  [Network.Mainnet]: "https://mainnet.streams.api.lworks.io",
-  [Network.Testnet]: "https://testnet.streams.api.lworks.io",
-};
