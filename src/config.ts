742465b6a2d1aee95c2f5168d2272ff9e24542db
Chad Schwab <chad.schwab@lworks.io>
2023-03-29 12:16:05 -0500

    update libraryVersion

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { Environment } from "./environment";
 import { Network } from "./networks";
 
-export const libraryVersion = "2.9.0";
+export const libraryVersion = "2.9.1";
 
 type Config = {
   disableTracking: boolean;
