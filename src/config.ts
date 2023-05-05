6fab1d54373641f6f175120326586eb422ad5a03
Chad Schwab <chad.schwab@lworks.io>
2023-05-05 13:24:01 -0500

    2.13.0, update prettier ignore

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { Environment } from "./environment";
 import { Network } from "./networks";
 
-export const libraryVersion = "2.13.0-alpha.7";
+export const libraryVersion = "2.13.0";
 
 type Config = {
   disableTracking: boolean;
