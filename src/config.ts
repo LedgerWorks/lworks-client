8d711d55cd00a850f2015ef9d1a955d277a73c3d
Cory Huebner <chuebner86@gmail.com>
2023-06-12 15:09:39 -0500

    DEV-2007 - publishing stable version; skipping sentinel tests for now

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { Environment } from "./environment";
 import { Network } from "./networks";
 
-export const libraryVersion = "3.4.1-alpha1";
+export const libraryVersion = "3.4.1";
 
 type Config = {
   disableTracking: boolean;
