59c2f9ca51568673ea0718a9e90c238be89933a2
Chad Schwab <chad.schwab@lworks.io>
2024-05-21 14:45:44 -0500

    6.0.0 release

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { Environment } from "./environment";
 import { Network } from "./networks";
 
-export const libraryVersion = "5.5.0";
+export const libraryVersion = "6.0.0";
 
 type Config = {
   network: null | Network;
